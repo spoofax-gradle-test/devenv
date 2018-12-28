bc1faea65acb669721555b725c05f665d6262011
Gabriel Konat <gabrielkonat@gmail.com>
2018-12-28 15:28:24 +0100

    Include builds for all subdirectories that are gradle projects. Add gitonium submodule.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 rootProject.name = "devenv"
 
-includeBuild("coronium")
-includeBuild("log")
-includeBuild("pie")
-includeBuild("spoofax")
-includeBuild("spoofax.eclipse")
+// Add all subdirectories with a settings.gradle(.kts) file as an included build.
+rootDir.list().forEach {
+  if(File(rootDir, "$it/${Settings.DEFAULT_SETTINGS_FILE}").isFile || File(rootDir, "$it/${Settings.DEFAULT_SETTINGS_FILE}.kts").isFile) {
+    includeBuild(it)
+  }
+}
