6292a52d4e3dfd646a29db064d5eceec13e6dc40
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-12 15:18:01 +0100

    Add jenkins.pipeline project.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
 rootProject.name = "devenv"
 
+pluginManagement {
+  repositories {
+    maven(url = "http://home.gohla.nl:8091/artifactory/all/")
+    gradlePluginPortal()
+  }
+}
+
 // Add all subdirectories with a settings.gradle(.kts) file as an included build.
 rootDir.list().forEach {
   if(File(rootDir, "$it/${Settings.DEFAULT_SETTINGS_FILE}").isFile || File(rootDir, "$it/${Settings.DEFAULT_SETTINGS_FILE}.kts").isFile) {
